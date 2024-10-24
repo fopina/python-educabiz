e0eda303596415fbaeba09e3ccb75a9494858f8b
Filipe Pina <hzlu1ot0@duck.com>
2024-10-24 19:20:17 +0100

    x

--- .github/sync_requires.py
+++ .github/sync_requires.py
@@ -13,26 +13,15 @@ def main():
     args = p.parse_args()
 
     def pipfile_to_pyproject(pipfile_path, pyproject_path):
-        # Read the Pipfile
         with open(pipfile_path, 'r') as f:
             pipfile_content = f.read()
+        with open(pyproject_path, 'r') as f:
+            pyproject_content = f.read()
 
-        # Parse the Pipfile content
+
         pipfile_data = toml.loads(pipfile_content)
-
-        # Create the pyproject.toml structure
-        pyproject_data = {
-            'tool': {
-                'poetry': {
-                    'name': pipfile_data.get('name', 'my-package'),
-                    'version': pipfile_data.get('version', '0.1.0'),
-                    'description': pipfile_data.get('description', ''),
-                    'authors': pipfile_data.get('authors', []),
-                    'dependencies': pipfile_data.get('packages', {}),
-                    'dev-dependencies': pipfile_data.get('dev-packages', {}),
-                }
-            }
-        }
+        pyproject_data = toml.loads(pyproject_content)
+        pyproject_data['project']['dependencies'] = pipfile_data.get('packages', {})
 
         # Write the pyproject.toml file
         with open(pyproject_path, 'w') as f:
