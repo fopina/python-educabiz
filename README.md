bbe55d9d83e26a2d884d9783ff9f45817b90c4a5
Filipe Pina <hzlu1ot0@duck.com>
2024-10-28 16:19:03 +0000

    x

--- README.md
+++ README.md
@@ -5,11 +5,37 @@ API client to interact with [educabiz.com](https://www.educabiz.com) portals.
 ## Install
 
 ```
-pip install ...WIP...
+pip install educabiz
 ```
 
 ## Usage
 
 ```python
-...snippet from cli_test?...
+from educabiz.client import Client
+
+eb_client = Client()
+eb_client.login(username, password)
+data = eb_client.home()
+print(f'School: {data["schoolname"]}')
+children = eb_client.school_qrcodeinfo()['child']
+for child in children.values():
+    child_id = child['id']
+    home_data = data['children'][child_id]
+    print(f'{child_id}:')
+    print(f'* Name: {html.unescape(child["name"])}')
+    print(f'* Photo URL: {home_data["photo"]}')
+    presence = child['presence'][0]
+    if presence['id'] == 'undefined':
+        presence_str = '(none)'
+    elif presence['absent']:
+        presence_str = f'absent ({presence["notes"]})'
+    elif presence['hourOut'] == '--:--':
+        presence_str = f'checked in at {presence["hourIn"]}'
+    else:
+        presence_str = f'checked in at {presence["hourIn"]} and out at {presence["hourOut"]}'
+    print(f'* Presence: {presence_str}')
 ```
+
+## Build
+
+Check out [CONTRIBUTING.md](CONTRIBUTING.md)
--- cli_test.py
+++ cli_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env -S python3 -u
 
+import html
 import os
 
 import dotenv
@@ -13,13 +14,23 @@ def cli():
     c.login(os.getenv('EDUCA_USERNAME'), os.getenv('EDUCA_PASSWORD'))
     data = c.home()
     print(f'School: {data["schoolname"]}')
-    for child_id, child in data['children'].items():
-        print(f'{child_id}:')
-        print(f'* Name: {child["name"]}')
     children = c.school_qrcodeinfo()['child']
     for child in children.values():
-        print(child)
-        assert len(child['presence']) == 1
+        child_id = child['id']
+        home_data = data['children'][child_id]
+        print(f'{child_id}:')
+        print(f'* Name: {html.unescape(child["name"])}')
+        print(f'* Photo URL: {home_data["photo"]}')
+        presence = child['presence'][0]
+        if presence['id'] == 'undefined':
+            presence_str = '(none)'
+        elif presence['absent']:
+            presence_str = f'absent ({presence["notes"]})'
+        elif presence['hourOut'] == '--:--':
+            presence_str = f'checked in at {presence["hourIn"]}'
+        else:
+            presence_str = f'checked in at {presence["hourIn"]} and out at {presence["hourOut"]}'
+        print(f'* Presence: {presence_str}')
 
 
 if __name__ == '__main__':
