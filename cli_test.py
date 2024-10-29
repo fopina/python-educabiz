75a2e7fe72c363efe5abef28f3c09419cdd5c430
Filipe Pina <hzlu1ot0@duck.com>
2024-10-29 04:20:12 +0000

    x

--- cli_test.py
+++ cli_test.py
@@ -13,15 +13,15 @@ def cli():
     c = Client(os.getenv('EDUCA_USERNAME'), os.getenv('EDUCA_PASSWORD'), login_if_required=True)
     data = c.home()
     print(f'School: {data.schoolname}')
-    children = c.school_qrcodeinfo()['child']
+    children = c.school_qrcodeinfo().child
     for child in children.values():
-        child_id = child['id']
+        child_id = child.id
         home_data = data.children[child_id]
         print(f'{child_id}:')
-        print(f'* Name: {html.unescape(child["name"])}')
+        print(f'* Name: {html.unescape(child.name)}')
         print(f'* Photo URL: {home_data.photo}')
-        presence = child['presence'][0]
-        if presence['id'] == 'undefined':
+        presence = child.presence[0]
+        if presence.id == 'undefined':
             presence_str = '(none)'
         elif presence['absent']:
             presence_str = f'absent ({presence["notes"]})'
