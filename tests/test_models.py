d97bf09bd5c0f8d6ca3491d56ebec614fac609b3
Filipe Pina <hzlu1ot0@duck.com>
2024-10-29 04:41:03 +0000

    x

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,9 +5,12 @@ class Test(unittest.TestCase):
     def test_presence(self):
         from educabiz.models.school_qrcodeinfo import PresenceItem
 
-        p = PresenceItem.model_validate({'id': '123123', 'notes': 'nice'})
+        p = PresenceItem.model_validate({'id': '123123', 'notes': 'nice', 'random': 'z', 'hourIn': '--:--'})
         self.assertEqual(p.id, '123123')
-        self.assertEqual(p.model_extra['notes'], 'nice')
+        self.assertEqual(p.notes, 'nice')
+        self.assertEqual(p.hourOut, '')
+        self.assertEqual(p.hourIn, '')
+        self.assertEqual(p.model_extra['random'], 'z')
 
     def test_child_name(self):
         from educabiz.models.school_qrcodeinfo import Child
